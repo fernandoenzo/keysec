2cce6771b6cf4cab99aa39e7ca357d979293231d
Fernando Guarini <fernandoenzo@gmail.com>
2022-08-16 01:53:38 +0200

    Updated version to 1.3.0

--- keysec/version.py
+++ keysec/version.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 # encoding:utf-8
 
-VERSION = '1.2.1'
-DATE = '2022 Aug 15'
+VERSION = '1.3.0'
+DATE = '2022 Aug 16'
 
 version_msg = f'''KeySec {VERSION} ({DATE})
 Copyright © 2022 Fernando Enzo Guarini
@@ -10,4 +10,4 @@ License GPLv3+: GNU GPL version 3 or later <https://gnu.org/licenses/gpl.html>.
 This is free software: you are free to change and redistribute it.
 There is NO WARRANTY, to the extent permitted by law.
 
-Written by Fernando Enzo Guarini'''
+Written by Fernando Enzo Guarini.'''
